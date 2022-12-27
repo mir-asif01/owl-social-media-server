03fc58aa23327c03eb7a9d28aec3e218978b1220
asif3062 <asifbsb2002@gmail.com>
2022-12-27 18:24:23 +0600

    database added

--- index.js
+++ index.js
@@ -3,10 +3,30 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
+/* 
+ db name = owl-app-admin
+ dbpass = KDru9ieff7loIVDU
+*/
+
 app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send("Owl social media server is running.")
+})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.mtnbd39.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/users',(req,res)=>{
+    res.send("This is users page")
 })
 
 app.listen(port,()=>{
